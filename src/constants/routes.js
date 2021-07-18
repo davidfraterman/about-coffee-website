7fccafc66cc75be6985e2b7063fcf483f5744457
David Fraterman <davidfraterman@gmail.com>
2021-07-18 23:35:11 +0200

    adsf

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 export const HOME = "/";
 export const ABOUT = "/about";
 export const BLOG = "/blog";
-export const PRODUCTS = "/products";
+export const PRODUCTS = "/producten";
 export const PRODUCT = "/products/:id";
 export const NOT_FOUND = "/not-found";
