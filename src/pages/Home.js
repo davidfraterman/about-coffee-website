1f8a9c099f32ba1c8cbee84c6413ca249fc92ec7
David Fraterman <davidfraterman@gmail.com>
2021-07-19 19:22:06 +0200

    sd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import ArrowLink from "../components/buttons/ArrowLink";
 import HomeHeader from "../components/HomeHeader";
 import AanbevolenProducten from "../components/aanbevolen/AanbevolenProducten";
+import Button from "../components/buttons/ProductenButton";
 // import Ratings from "../components/Ratings";
 import * as ROUTES from "../constants/routes";
 import "./Home.css";
 
-import boer from "../images/boer.JPG";
+import shoppingcart from "../images/shoppingcart.png";
+import boer from "../images/boer.jpg";
 
 const Home = () => {
   useEffect(() => {
@@ -25,7 +28,7 @@ const Home = () => {
           Hoge kwaliteit koffie speciaal gemaakt en afgesteld door de boer zelf
           voor een perfecte smaak.
         </p>
-        <ArrowLink text="Over ons" />
+        <ArrowLink text="Over ons" to={ROUTES.ABOUT} />
       </div>
 
       <AanbevolenProducten />
@@ -45,6 +48,50 @@ const Home = () => {
         </div>
         <div className="section-image-img">
           <img src={boer} alt="boer" loading="lazy" />
+        </div>
+      </div>
+
+      <div className="home section-plain">
+        <h2>ABOUT DIRECT & FAIR TRADE</h2>
+        <h1>Verantwoord, en rechtstreeks van de boer</h1>
+        <p>
+          Hoge kwaliteit koffie speciaal gemaakt en afgesteld door de boer zelf
+          voor een perfecte smaak.
+        </p>
+        <Link to="/producten">
+          <Button type="producten">
+            <div className="button-content-wrapper">
+              <img
+                className="button-icon"
+                src={shoppingcart}
+                alt="shopping cart icon"
+              />
+              Producten
+            </div>
+          </Button>
+        </Link>{" "}
+      </div>
+
+      <div className="home section-blog">
+        <div className="aanbevolen-article">
+          <h2>BLOG</h2>
+          <div className="aanbevolen-article-img-wrapper">
+            <img className="aanbevolen-article-img" src={boer} alt="blog" />
+          </div>
+          <h1>Hoe onze producten fair trade zijn</h1>
+          <p>
+            Hoge kwaliteit rechtstreeks vanuit Colombia in uw keuken. Bestel nu
+            voor 24% korting.
+          </p>
+
+          <ArrowLink to={ROUTES.BLOG} text="Lees hier over" />
+        </div>
+      </div>
+
+      <div className="home section-questions">
+        <div className="home section-questions-content">
+          <h1>Nog vragen?</h1>
+          <ArrowLink to="mailto:davidspam228@gmail.com" text="Contact" />
         </div>
       </div>
     </>
